e542e94f39d0074db80d3980ce6b6b8d9950bb35
Nicocchi <nicocchilover@gmail.com>
2018-10-29 14:14:44 -0400

    Refactored server PORT to env file

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 const logger = require('./modules/Logger.js');
-const port = 8000;
 
+const port = process.env.PORT || 8000;
+
+/**
+ * Log the server start status
+ */
 function log() {
     logger.log(`Starting server`);
     logger.log(`Server started on port: ${port}`, 'ready');
 }
+
 server.listen(port, log());
