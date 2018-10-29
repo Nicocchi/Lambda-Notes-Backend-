07b5c933446fe4de74deb6e35e5c112252105cc7
Nicocchi <nicocchilover@gmail.com>
2018-10-29 13:32:09 -0400

    added notes database

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/dev/database/notes.sqlite3'
+      filename: './data/dev/notes.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
